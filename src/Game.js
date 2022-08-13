44783725181dcda87eebc8870a4924f17bd40556
Ashish Tiwari <ashish@novo.cloud>
2022-08-13 15:15:59 +0530

    Added backedn link

--- src/Game.js
+++ src/Game.js
@@ -7,7 +7,7 @@ import { useRef, useState } from "react";
 import Onboard from "./Onboard";
 
 function Game() {
-  const socket = io.connect("http://localhost:3001");
+  const socket = io.connect("https://socket-server-chess.herokuapp.com");
   const [fen, setFen] = useState("start");
   const [gameStarted, setGameStarted] = useState(false);
   let params = useParams();
